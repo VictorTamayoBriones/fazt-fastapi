8f3637b27f90215bda5f59084b01420f1a827fb8
VictorTamayoBriones <66786892+VictorTamayoBriones@users.noreply.github.com>
2022-03-07 22:32:59 -0600

    get post by id

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from typing import Text, Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from uuid import uuid4 as uuid
 
@@ -32,4 +32,12 @@ def get_posts():
 def save_post(post: Post):
     post.id = str(uuid())
     posts.append(post.dict())
-    return "Recived"
+    return posts[-1]
+
+#Obtener un post por ID
+@app.get('/posts/{post_id}')
+def get_post(post_id: str):
+    for post in posts:
+        if post["id"] == post_id:
+            return post
+    raise HTTPException(status_code=404, detail="Post Not Found")
